b6e2f799512849fe258f03153c1ea687c3d928f1
lululufr <lucas.lm.miller@gmail.com>
2025-01-17 12:50:07 +0100

    ADD build

--- main.s
+++ main.s
@@ -1,6 +1,8 @@
 global _start 
 
 section .data
+
+
 address:
   dw 2
   dw 0
@@ -27,6 +29,10 @@ buffer:
 good:
   db 'good'
 
+
+section .bss
+  tmp resb 64
+
 section .text
 
 _start:
@@ -47,17 +53,10 @@ _start:
   mov r9, 16
   syscall
 
-  mov rax, 45
-  mov rdi, r12
-  mov rsi, buffer
-  mov rdx, 1024
-  mov r10, 0
-  mov r8, 0
-  mov r9, 0
-  syscall
+  mov rax, [buffer]
+  mov rdi,tmp 
+  call itoa
 
-
-success:
 
   mov rax, 1
   mov rdi, 1
@@ -69,9 +68,50 @@ success:
   mov rdi, 0
   syscall
 
-failure: 
-  mov rax, 60
-  mov rdi, 1
-  syscall
 
 
+itoa:
+    ; Paramètres d'entrée :
+    ; EAX : le nombre à convertir
+    ; RDX : adresse de la chaîne de sortie (tampon)
+
+    push rbx                ; Sauvegarder EBX
+    push rsi                ; Sauvegarder ESI
+    push rdi                ; Sauvegarder EDI
+
+    mov rdi, rdx            ; EDI pointe vers le tampon de sortie
+    mov rcx, 0              ; ECX compte les caractères
+    test rax, rax           ; Tester si le nombre est négatif
+    jge .positive           ; Si positif, sauter à la partie positive
+
+    ; Gérer le signe négatif
+    mov byte [rdi], '-'     ; Ajouter '-' au tampon
+    inc rdi                 ; Avancer le pointeur
+    neg rax                 ; Rendre le nombre positif
+
+.positive:
+    ; Conversion du nombre en base 10
+    mov rbx, 10             ; Diviseur (base 10)
+.next_digit:
+    xor rdx, rdx            ; Effacer EDX pour éviter les restes
+    div rbx                 ; Diviser EAX par 10, quotient dans EAX, reste dans EDX
+    add dl, '0'             ; Convertir le chiffre en caractère ASCII
+    push rdx                 ; Empiler le caractère
+    inc rcx                 ; Incrémenter le compteur de caractères
+    test rax, rax           ; Vérifier si EAX est zéro
+    jnz .next_digit         ; Reboucler s'il reste des chiffres
+
+    ; Écrire les chiffres au tampon
+.write_digits:
+    pop rax                  ; Dépiler un caractère
+    mov [rdi], al           ; Écrire dans le tampon
+    inc rdi                 ; Avancer le pointeur
+    loop .write_digits      ; Répéter jusqu'à ce que tous les caractères soient écrits
+
+    ; Ajouter le caractère de fin de chaîne
+    mov byte [rdi], 0       ; Terminaison de chaîne avec 0
+
+    pop rdi                 ; Restaurer les registres
+    pop rsi
+    pop rbx
+    ret                     ; Retour
